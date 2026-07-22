72ea208489e4ab2848cda4b3cc2d95cfb5a992b6
GAlbina123 <mrdecidueyelive@gmail.com>
2026-07-22 21:46:26 +0300

    Updated character movement

--- GMTk-gamejam/prefabs/player.gd
+++ GMTk-gamejam/prefabs/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 300
+var direction = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,17 +11,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	velocity = Vector2.ZERO
+	direction = Vector2.ZERO
 	
 	if Input.is_action_pressed("up"):
-		velocity.y = -1
+		direction.y = -1
 	if Input.is_action_pressed("down"):
-		velocity.y = 1
+		direction.y = 1
 	if Input.is_action_pressed("right"):
-		velocity.x = 1
+		direction.x = 1
 	if Input.is_action_pressed("left"):
-		velocity.x = -1
+		direction.x = -1
 	
 	#print(velocity)
 	
-	velocity = velocity.normalized() * speed
+	velocity = direction.normalized() * speed
 	move_and_slide()
